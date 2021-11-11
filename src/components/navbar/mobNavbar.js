b1491352b098911b33b896fba666dfe9fffdb051
Nikel Neel <nike007143@gmail.com>
2021-11-11 19:52:05 +0530

    Hamburger menu fix

--- src/components/navbar/mobNavbar.js
+++ src/components/navbar/mobNavbar.js
@@ -1,14 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./navStyle.module.css";
 import styled from "styled-components";
-import { MenuToggle } from "./menuToggle";
 import Slide from '@material-ui/core/Slide';
 
 
 const NavLinksContainer = styled.div`
   height: 100%;
   display: flex;
-  align-items: center;
   overflow: hidden;
 `;
 
@@ -31,32 +29,33 @@ const LinksWrapper = styled.ul`
 const navList = ["About", "Work", "Skills", "Experience", "Contact"];
 
 const MobileNavLinks = () => {
-    const [isOpen, setOpen] = useState(false);
-    const handleClick = (e) => {
-      setTimeout(function(){
+  const [isOpen, setOpen] = useState(false);
+  const handleClick = (e) => {
+    setTimeout(function () {
       e.preventDefault();
       setOpen(false)
-      },100
-      ) 
-    };
-    return (
-      <NavLinksContainer>
-        <MenuToggle isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
-        {isOpen && (
+    }, 100
+    )
+  };
+  return (
+    <NavLinksContainer>
+      <i className={`fas fa-bars fa-2x`} style={{ color: 'bisque' }} isOpen={isOpen} onClick={() => setOpen(!isOpen)} ></i>
+      {isOpen && (
         <Slide direction={'left'} in={true} mountOnEnter unmountOnExit>
           <LinksWrapper>
+            <i className={`fas fa-bars fa-2x`} isOpen={isOpen} onClick={() => setOpen(!isOpen)} ></i>
             {navList.map((item, index) => (
-            <li key={index + 1} className={styles.navLink} style={{padding:"20px"}}>
-              <a href={`#${item}`} onClick={handleClick}>{item}</a>
-            </li>
-          ))}
-          <div className="div-resume"><a className="resume-download" href="./resume/nikel.pdf" download>Resume</a></div>
+              <li key={index + 1} className={styles.navLink} style={{ padding: "20px" }}>
+                <a href={`#${item}`} onClick={handleClick}>{item}</a>
+              </li>
+            ))}
+            <div className="div-resume"><a className="resume-download" href="./resume/nikel.pdf" download>Resume</a></div>
           </LinksWrapper>
         </Slide>
-        )}
-      </NavLinksContainer>
-    );
-  }
+      )}
+    </NavLinksContainer>
+  );
+}
 
 
 export default MobileNavLinks;
