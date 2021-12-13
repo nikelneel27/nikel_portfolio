797631d920e52fad168adcc0321dfc83d4d9048d
Nikel Neel <nike007143@gmail.com>
2021-12-13 11:01:02 +0530

    added project links

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -15,7 +15,7 @@ const Projects = () => {
                     <a href="#" className="featured-title">Dunder Mifflin</a>
                     <p className="featured-desc">The purpose of this app helps the HR to build a detailed joining offer letter that would be given to the suitable candidate ie.  Position offered, complete breakdown of salary details, benefits, equity. </p>
 
-                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue, Charts used- Echart </p>
+                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue, Charts used- Echart <p><strong>Link :</strong><a href="https://dunder-mil.netlify.app/" target="_blank">https://dunder-mil.netlify.app/</a></p> </p>
                 </div>
             </div>
             <a href="https://dunder-mil.netlify.app/" target="_blank"> <img src={dunderMifflin}
@@ -45,10 +45,10 @@ const Projects = () => {
                     <p className="featured-desc">This app was built to help pet parents  create and register for any pet meet up that is available. It can also be used for conducting adoption drive for Animal  shelters.
 
 </p>
-                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue,  Vuetify, Firebase</p>
+                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue,  Vuetify, Firebase <p><strong>Link :</strong><a href="https://pets-meetup-vue.netlify.app/" target="_blank">https://pets-meetup-vue.netlify.app/</a></p></p>
                 </div>
             </div>
-            <a href="https://image-search-unsplash-api.netlify.app/" target="_blank"> <img src={petsmeetup}
+            <a href="https://pets-meetup-vue.netlify.app/" target="_blank"> <img src={petsmeetup}
                 alt="featured" /></a>
             <div className="section-border"></div>
 
@@ -61,7 +61,10 @@ const Projects = () => {
                     <p className="featured-desc">This app helps in finding beautiful high resolution images of the item you are searching for. Its similar to the unsplash website, which helps Photographers to showcase their work.
 
 </p>
-                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue, Bootstrap, Unsplash Apis, Firebase</p>
+                    <p className="featured-desc">Technologies used :  HTML, CSS, Java Script, Vue, Bootstrap, Unsplash Apis, Firebase
+                    <p><strong>Link :</strong><a href="https://image-search-unsplash-api.netlify.app/" target="_blank">https://image-search-unsplash-api.netlify.app/</a></p></p>
+
+
                 </div>
             </div>
             <a href="https://image-search-unsplash-api.netlify.app/" target="_blank"> <img src={ImageSearch}
